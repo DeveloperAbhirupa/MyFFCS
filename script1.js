646ec6875341e912a519896bf8245824d652eb39
DeveloperAbhirupa <abhirupa.mitra2017@vitstudent.ac.in>
2018-05-06 02:18:40 +0530

    Selected courses js modified

--- script1.js
+++ script1.js
@@ -12,15 +12,17 @@ function access() {
 }
 
 
-var venue;//Hardcoded data to be replaced by JSON
+var venue="SJT 305";//Hardcoded data to be replaced by JSON
 var courseCode="CSE2001";//Hardcoded data to be replaced by JSON
-var courseTitle;//Hardcoded data to be replaced by JSON
+var courseTitle="Introduction To Python";//Hardcoded data to be replaced by JSON
 var type="LAB";//Hardcoded data to be replaced by JSON
 var slot="L33+L36+L50+L51+L10+L11"; //Hardcoded data to be replaced by JSON
+var c=4; //Hardcoded data to be replaced by JSON
 var slotInit=slot; //Copying values
-var faculty;//Hardcoded data to be replaced by JSON
+var faculty="Dr. Rajkumar S";//Hardcoded data to be replaced by JSON
 var slotName=".";
 
+addDataToList(slotInit, courseCode, courseTitle, venue, faculty, c);
 // console.log(length);
 
 
@@ -65,10 +67,32 @@ changeSlotColor(".C2", "MAT2002");
 
 // console.log(slotName,"slotName");
 function changeSlotColor(s, code) {
-    var slotInit= s.substr(1, s.length);
+    var slotI= s.substr(1, s.length);
     $(s).addClass(type);
-    $(s).html(code+"-"+ '<br/>'+slotInit);
+    $(s).html(code+"-"+ '<br/>'+slotI);
+
 }
+
+function addDataToList(s,c,t,v,f,cd)
+{
+    var table = document.getElementById("sec_Course");
+    var row=table.insertRow(1);
+    var slot=row.insertCell(0);
+    var code=row.insertCell(1);
+    var title=row.insertCell(2);
+    var ven=row.insertCell(3);
+    var facl=row.insertCell(4);
+    var cred=row.insertCell(5);
+    slot.innerHTML=s;
+    code.innerHTML=c;
+    title.innerHTML=t;
+    ven.innerHTML=v;
+    facl.innerHTML=f;
+    cred.innerHTML=cd;
+
+}
+
+
 
 changeSlotColor();
 
